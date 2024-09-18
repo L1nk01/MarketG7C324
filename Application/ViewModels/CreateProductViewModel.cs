972b66d79314f2c57bfa8bfeb6d150b17aa37f89
L1nk01 <ecampospaulino@gmail.com>
2024-09-18 19:23:43 -0400

    Added default value for Name attribute

--- Application/ViewModels/CreateProductViewModel.cs
+++ Application/ViewModels/CreateProductViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class CreateProductViewModel
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public double Price { get; set; }
         public int Category {  get; set; }
--- Application/ViewModels/ProductViewModel.cs
+++ Application/ViewModels/ProductViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class ProductViewModel
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public double Price { get; set; }
     }
